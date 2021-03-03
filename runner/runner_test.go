c8fac1a77876e66471256ea7af735d517fe4c5fd
k1low <k1low@pepabo.com>
2021-03-03 10:45:46 +0900

    Fix sampleByEnv

--- runner/runner_test.go
+++ runner/runner_test.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -30,6 +31,34 @@ func TestDetectTargetNumber(t *testing.T) {
 		}
 		if gotState != tt.wantState {
 			t.Errorf("got %v\nwant %v", gotState, tt.wantState)
+		}
+	}
+}
+
+func TestSampleByEnv(t *testing.T) {
+	tests := []struct {
+		env  int
+		want int
+	}{
+		{3, 3},
+		{2, 2},
+		{4, 3},
+		{0, 0},
+	}
+	r, err := New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, tt := range tests {
+		in := []string{"alice", "bob", "charlie"}
+		envKey := "TEST_SAMPLE_BY_ENV"
+		os.Setenv(envKey, fmt.Sprintf("%d", tt.env))
+		got, err := r.sampleByEnv(in, envKey)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(got) != tt.want {
+			t.Errorf("got %v\nwant %v", got, tt.want)
 		}
 	}
 }
