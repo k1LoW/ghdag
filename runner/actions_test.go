06e98b2f7b7803deaa3c7be310d1fdaecd54014a
k1LoW <k1lowxb@gmail.com>
2021-03-07 22:10:43 +0900

    Add test for PerformRunAction

--- runner/actions_test.go
+++ runner/actions_test.go
@@ -3,6 +3,7 @@ package runner
 import (
 	"context"
 	"errors"
+	"os"
 	"testing"
 
 	"github.com/bxcodec/faker/v3"
@@ -12,6 +13,48 @@ import (
 	"github.com/k1LoW/ghdag/target"
 )
 
+func TestPerformRunAction(t *testing.T) {
+	r, err := New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := r.revertEnv(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	tests := []struct {
+		in         string
+		wantStdout string
+		wantStderr string
+		wantErr    bool
+	}{
+		{"echo hello", "hello\n", "", false},
+		{"echo world 1>&2", "", "world\n", false},
+		{"unknowncmd", "", "", true},
+	}
+	for _, tt := range tests {
+		if err := r.revertEnv(); err != nil {
+			t.Fatal(err)
+		}
+		ctx := context.Background()
+		i := &target.Target{}
+		if err := faker.FakeData(i); err != nil {
+			t.Fatal(err)
+		}
+		if err := r.PerformRunAction(ctx, i, tt.in); (err == nil) == tt.wantErr {
+			t.Errorf("got %v\nwantErr %v", err, tt.wantErr)
+		}
+		if got := os.Getenv("GHDAG_ACTION_RUN_STDOUT"); got != tt.wantStdout {
+			t.Errorf("got %v\nwant %v", got, tt.wantStdout)
+		}
+		if got := os.Getenv("GHDAG_ACTION_RUN_STDERR"); got != tt.wantStderr {
+			t.Errorf("got %v\nwant %v", got, tt.wantStdout)
+		}
+	}
+}
+
 func TestPerformLabelsAction(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -20,6 +63,12 @@ func TestPerformLabelsAction(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer func() {
+		if err := r.revertEnv(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
 	m := mock.NewMockGitHubClient(ctrl)
 	r.github = m
 
@@ -33,10 +82,12 @@ func TestPerformLabelsAction(t *testing.T) {
 		{[]string{"bug", "question"}, []string{"bug", "question"}, []string{"bug", "question"}, &erro.AlreadyInStateError{}},
 	}
 	for _, tt := range tests {
-		r.revertEnv()
+		if err := r.revertEnv(); err != nil {
+			t.Fatal(err)
+		}
 		ctx := context.Background()
-		i := target.Target{}
-		if err := faker.FakeData(&i); err != nil {
+		i := &target.Target{}
+		if err := faker.FakeData(i); err != nil {
 			t.Fatal(err)
 		}
 		if tt.current != nil {
@@ -44,11 +95,11 @@ func TestPerformLabelsAction(t *testing.T) {
 		}
 		if tt.wantErr == nil {
 			m.EXPECT().SetLabels(gomock.Eq(ctx), gomock.Eq(i.Number), gomock.Eq(tt.want)).Return(nil)
-			if err := r.PerformLabelsAction(ctx, &i, tt.in); err != nil {
+			if err := r.PerformLabelsAction(ctx, i, tt.in); err != nil {
 				t.Error(err)
 			}
 		} else {
-			if err := r.PerformLabelsAction(ctx, &i, tt.in); !errors.As(err, tt.wantErr) {
+			if err := r.PerformLabelsAction(ctx, i, tt.in); !errors.As(err, tt.wantErr) {
 				t.Errorf("got %v\nwant %v", err, tt.wantErr)
 			}
 		}
